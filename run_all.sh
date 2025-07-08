83a72d0d8d8d72ed40c17c5c8ae3e9da29ba0794
Ashmit0 <ashbathla06@gmail.com>
2025-07-08 08:20:01 +0000

    bug fixes

---
+++ run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+underlyings=("HFCL" "DIXON")
+dates=("20250519" "20250520" "20250521" "20250522" "20250523")
+
+for u in "${underlyings[@]}"; do
+    for d in "${dates[@]}"; do
+        echo "Running: $u on $d"
+        python script.py "$u" "$d"
+    done
+done
